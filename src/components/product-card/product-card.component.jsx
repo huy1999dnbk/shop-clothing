1273cf1b7a108c12420302f0c4ee77efdaa5f0c5
huy1999dnbk <minhhuy99dn@gmail.com>
2022-09-08 21:38:42 +0700

    add addToCart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,24 @@
+import { useContext } from "react";
 import "./product-card.styles.scss";
-
+import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
-      <div className="footer">
+      <div className="footer">s
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted">Add to card</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to card
+      </Button>
     </div>
   );
 };
